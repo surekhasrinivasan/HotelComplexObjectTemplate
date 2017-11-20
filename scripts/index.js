00cc27cf7f6a5683083ea4ffb27800e4e4ae4a9a
surekha <surekhasrao@gmail.com>
2017-11-20 17:50:33 +0000

    Display the rooms from the Hotel Object as Radio buttons

--- scripts/index.js
+++ scripts/index.js
@@ -18,4 +18,22 @@ var hotel = {
     ],
     name: "CareerDevs Innt"
 }
+//display rooms as radio options 
+//console.log(document.getElementById("label").nodeName);
+for(var i =0; i<hotel.rooms.length; i++){
+    console.log("start");
+    var radioBtn = document.createElement("INPUT");
+    var radioLbl = document.createElement("LABEL");
+    radioBtn.setAttribute("type", "radio");
+    radioBtn.setAttribute("name", "rooms");
+    radioBtn.setAttribute("value", i);
+    radioBtn.setAttribute("id","room" + i);
+    radioLbl.innerHTML = hotel.rooms[i].name;
+    console.log("stop");
+    
+    document.getElementById("radialSection").appendChild(radioBtn);
+    document.getElementById("radialSection").appendChild(radioLbl);
+}
 
+// on form submission confirm radio was selected 
+// and checkbox checked 
